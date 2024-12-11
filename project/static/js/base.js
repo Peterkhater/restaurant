86be4a90b36150bea05416ca50cebda904189f1b
peter_khater <peter.2006khater@gmail.com>
2024-12-11 19:34:28 +0200

    adding the preloader

--- project/static/js/base.js
+++ project/static/js/base.js
@@ -6,3 +6,32 @@ document.addEventListener("DOMContentLoaded", function () {
         navBottom.classList.toggle("active");
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const preloader = document.querySelector('.pl'); 
+    const navContainer = document.querySelector('.nav_container');
+    const mainContentContainer = document.querySelector('.allContent');
+    
+    // Hide nav and content while loading
+    navContainer.style.opacity = '0';
+    mainContentContainer.style.opacity = '0';
+
+    window.addEventListener('load', () => {
+        
+        preloader.style.transition = 'opacity 0.5s ease';
+        preloader.style.opacity = '0';
+
+        
+        navContainer.style.transition = 'opacity 0.5s ease';
+        mainContentContainer.style.transition = 'opacity 0.5s ease';
+        
+        navContainer.style.opacity = '1';
+        mainContentContainer.style.opacity = '1';
+
+        setTimeout(() => {
+            preloader.style.display = 'none'; // Hide preloader after fade-out
+        }, 500); // Duration matches the fade-out transition
+    });
+});
+
